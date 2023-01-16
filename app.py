f11e9affb0a5d739cb7356eb2edf25cbb39f4d1f
Boris <betttttt@yandex.ru>
2023-01-16 21:13:28 +0300

    flask

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin  
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# app.config['SECRET_KEY'] = 'thisissec'
-
+app.config['SECRET_KEY'] = 'thisissec'
 
 login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message = "авторизируйтесь, чтобы получить доступ к недоступным странцам"
+login_manager.login_view = 'login'
+login_manager.login_message = "авторизируйтесь, чтобы получить доступ к недоступным странцам"
+
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 class User(db.Model):
@@ -19,30 +20,30 @@ class User(db.Model):
     email = db.Column(db.String(20), nullable = False)
     password = db.Column(db.String(300), nullable = False)
         
-# class UserLogin():
-#     def fromDB(self, user_id, db):
-#         if (len(db.session.query(User).filter(User.id == user_id).all()) > 0):
-#             self.__user = db.session.query(User).filter(User.id == user_id).all()[0]
-#             return self
-#         else:
-#             print("пользователь не найден")
-#             return False
-#     def create(self, user):
-#         self.__user = user
-#         return self
-#     def is_authenticated(self):
-#         return True
-#     def is_active(self):
-#         return True
-#     def if_anonymous(self):
-#         return False
-#     def get_id(self):
-#         return str(self.__user.id)
+class UserLogin(): # если написать db.Model то ломается, а если написать UserMixin и убрать 3 функции тоже ломается
+    def fromDB(self, user_id, db):
+        if (len(db.session.query(User).filter(User.id == user_id).all()) > 0):
+            self.__user = db.session.query(User).filter(User.id == user_id).all()[0]
+            return self
+        else:
+            print("пользователь не найден")
+            return False
+    def create(self, user):
+        self.__user = user
+        return self
+    def is_authenticated(self):   
+        return True
+    def is_active(self):
+        return True
+    def if_anonymous(self):
+        return False
+    def get_id(self):
+        return str(self.__user.id)
    
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#    return UserLogin().fromDB(user_id, db)   
+@login_manager.user_loader
+def load_user(user_id):
+   return UserLogin().fromDB(user_id, db)   
 
 @app.route('/')
 @login_required 
@@ -69,14 +70,14 @@ def register():
 @app.route('/profile')
 @login_required
 def profile():
-    # cust_id = current_user.get_id()
-    return render_template("profile.html", cust_id = 5)
+    cust_id = current_user.get_id()
+    return render_template("profile.html", cust_id = cust_id)
     
 
 @app.route('/login', methods = ["POST", "GET"])
 def login():
-    # if current_user.is_authenticated:
-    #     return redirect('/profile')
+    if current_user.is_authenticated:
+        return redirect('/profile')
     if (request.method == "POST"):
         tit = request.form['title1']
         passw = request.form['intro1']
@@ -85,22 +86,20 @@ def login():
         else:
             return "mistake"
         if check_password_hash(user.password, passw):
-            # userlogin = UserLogin().create(user)
-            # rm = True if request.form.get('remember') else False
-            # login_user(userlogin, remember=rm)
-            # return redirect(request.args.get("next") or '/profile')
-            return redirect('/profile')
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get('remember') else False
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get("next") or '/profile')
         return "mistake"
     else:
         return render_template("login.html")
 
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash("you are logout")
-#     return redirect("/login")
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("you are logout")
+    return redirect("/login")
 
 if __name__ == '__main__':
-    
     app.run()
